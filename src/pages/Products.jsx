172032a7ffd2b971f5675c1ed34b5cf9042c5322
SangithaShri <137738934+SangithaShri@users.noreply.github.com>
2025-04-16 15:38:09 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Footer, Navbar, Product } from "../components"
-import Checkout from './Checkout';
 
 const Products = () => {
   return (
     <>
       <Navbar />
       <Product />
-      <Checkout/>
       <Footer />
     </>
   )
